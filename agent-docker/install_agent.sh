a69c818554c0a51bafb498e69e2a7c811fcfde67
Tagir Ismagilov <ismagilov.tagir@gmail.com>
2021-05-17 16:42:28 +0500

    Update agent to v2.186.1 (ubuntu 20.04)

--- agent-docker/install_agent.sh
+++ agent-docker/install_agent.sh
@@ -1,4 +1,4 @@
 echo "Downloading and installing Azure Pipelines agent..."
 
-curl -LS https://vstsagentpackage.azureedge.net/agent/2.185.1/vsts-agent-linux-x64-2.185.1.tar.gz | tar -xz & wait $!
+curl -LS https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz | tar -xz & wait $!
 ./bin/installdependencies.sh
