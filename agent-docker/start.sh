ae9c75854b784019e3bb63ff0771a14c68da708c
Tagir Ismagilov <ismagilov.tagir@gmail.com>
2021-06-08 12:05:06 +0500

    Update to v2.187.2 (downgrade linux to 18.04)

--- agent-docker/start.sh
+++ agent-docker/start.sh
@@ -88,7 +88,6 @@ print_header "3. Configuring Azure Pipelines agent..."
   --pool "${AZP_POOL:-Default}" \
   --work "${AZP_WORK:-_work}" \
   --replace \
-  --sslskipcertvalidation \
   --acceptTeeEula & wait $!
 
 print_header "4. Running Azure Pipelines agent..."
