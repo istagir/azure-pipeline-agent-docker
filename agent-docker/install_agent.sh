98505b8ebc00d54ad2856cdd10a5ab12c044b19e
Tagir Ismagilov <ismagilov.tagir@gmail.com>
2021-06-08 12:05:06 +0500

    Update to v2.187.2 (downgrade linux to 18.04)

--- agent-docker/install_agent.sh
+++ agent-docker/install_agent.sh
@@ -1,4 +1,4 @@
 echo "Downloading and installing Azure Pipelines agent..."
 
-curl -LS https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz | tar -xz & wait $!
+curl -LS https://vstsagentpackage.azureedge.net/agent/2.187.2/vsts-agent-linux-x64-2.187.2.tar.gz | tar -xz & wait $!
 ./bin/installdependencies.sh
